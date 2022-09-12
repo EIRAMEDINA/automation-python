25ea31e572b4a1374b7bbee68e4e847afac0e0ea
@EiraMedina <eira_medina@hotmail.com>
2022-09-12 14:21:26 -0500

    [ADD] registro codigo python

---
+++ MercuryTools.py
@@ -0,0 +1,35 @@
+from lib2to3.pgen2.token import NAME
+import time
+from xml.dom.minidom import Element
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+
+
+
+driver = webdriver.Chrome(executable_path='C:\\Users\\Eira Medina\\Documents\\automation-python\\drivers\\chromedriver.exe')
+driver.implicitly_wait(10)
+driver.maximize_window()
+    
+driver.get('https://demo.guru99.com/test/newtours/')
+
+#registro#
+
+Element = driver.find_element(By.LINK_TEXT, "REGISTER").click()
+Element = driver.find_element(By.NAME, "firstName").send_keys('PRUEBA')
+Element = driver.find_element(By.NAME, "lastName").send_keys('PYTHON')
+Element = driver.find_element(By.NAME , "phone").send_keys('5512345678')
+Element = driver.find_element(By.ID , "userName").send_keys('P.PYTHON')
+Element = driver.find_element(By.NAME , "address1").send_keys('BENUSIANO CARRANZA')
+Element = driver.find_element(By.NAME , "city").send_keys('MÉXICO')
+Element = driver.find_element(By.NAME , "state").send_keys('DISTRITO FEDERAL')
+Element = driver.find_element(By.NAME , "postalCode").send_keys('01800')
+
+select = Select(driver.find_element(By.NAME, "country"))
+select.select_by_visible_text('MEXICO')
+    
+Element = driver.find_element(By.NAME , "email").send_keys('P.PYTHON@gmail.COM')
+Element = driver.find_element(By.NAME , "password").send_keys('123456')
+Element = driver.find_element(By.NAME , "confirmPassword").send_keys('123456')
--- googleSearch.py
+++ googleSearch.py
@@ -1,11 +1,13 @@
+from xml.dom.minidom import Element
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path='C:\\Users\\Eira Medina\\Documents\\automation-python\\drivers\\chromedriver.exe')
 driver.implicitly_wait(10)
 driver.maximize_window()
 driver.get("https://www.google.com.mx/?hl=es-419")
-driver.find_element_by_name("q").send_keys("python")
-driver.find_element_by_name("btnK").click()
-driver.close()
+Element = driver.find_element(By.NAME, "q").send_keys('python')
+Element = driver.find_element(By.NAME, "btnK").click()
 driver.quit()
 print("Test complete PASS")
